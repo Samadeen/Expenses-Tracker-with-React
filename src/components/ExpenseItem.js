9e69eeafc1309c1883ee57067476789586775dbc
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-03-13 11:08:46 +0000

    Created Expense Item CSS component for styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
   return (
-    <div>
-      <div>Date</div>
+    <div className='expense-item'>
+      <div>13 March,2022</div>
       <div>
-        <h2>Title</h2>
+        <h2 className='expense-item__description'>Travelling Money</h2>
       </div>
-      <div>Amount</div>
+      <div className='expense-item__price'>50,000</div>
     </div>
   );
 }
