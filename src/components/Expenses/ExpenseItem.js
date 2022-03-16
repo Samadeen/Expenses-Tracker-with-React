48db7344f9d224023ab84b9a666925fd103faabb
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-03-16 16:09:14 +0000

    Expense Filter with State and Stateless

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated!!');
-  };
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div>
-        <h2 className='expense-item__description'>{title}</h2>
+        <h2 className='expense-item__description'>{props.title}</h2>
       </div>
       <div className='expense-item__price'>{props.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
